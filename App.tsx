7685e091cb46a6dad34e011fc588c0b502d1aecf
Loic Dal Zotto <loic.dalzotto@magicplan.app>
2024-04-11 11:33:36 +0200

    Commit

--- App.tsx
+++ App.tsx
@@ -1,118 +1,134 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { useEffect, useRef, useState } from 'react';
+import { Button, Image, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Camera, Orientation, useCameraDevice } from 'react-native-vision-camera';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+type PhotoModel = {
+    uri: string;
+    visionCameraWidth: number;
+    visionCameraHeight: number;
+    imageWidth: number;
+    imageheight: number;
+};
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default App = () => {
+    const [init, setInit] = useState(false);
+    useEffect(() => {
+        (async () => {
+            const newCameraPermission = await Camera.requestCameraPermission();
+            setInit(true);
+        })();
+    }, []);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+    const device = useCameraDevice('back');
+    const cameraRef = useRef<Camera>(null);
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+    const [orientation, setOrientation] = useState<Orientation>('portrait');
+    const [images, setImages] = useState<PhotoModel[]>([]);
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+    if (!init) {
+        return <></>;
+    }
+    if (device == null) return <></>;
+    return (
+        <View style={StyleSheet.absoluteFill}>
+            <Camera
+                ref={cameraRef}
+                style={StyleSheet.absoluteFill}
+                device={device}
+                isActive={true}
+                photo={true}
+                orientation={orientation}
+            />
+            <Button
+                title="Take photo"
+                onPress={async () => {
+                    const photoResult = await cameraRef.current?.takePhoto({});
+                    if (photoResult) {
+                        Image.getSize('file:///' + photoResult?.path, (w, h) => {
+                            setImages((prev) => {
+                                return [
+                                    ...prev,
+                                    {
+                                        uri: 'file:///' + photoResult.path,
+                                        visionCameraWidth: photoResult.width,
+                                        visionCameraHeight: photoResult.height,
+                                        imageWidth: w,
+                                        imageheight: h,
+                                    },
+                                ];
+                            });
+                        });
+                    }
+                }}
+            />
+            <View style={{ backgroundColor: 'red', height: 30 }}>
+                <View style={{ flex: 1, flexDirection: 'row' }}>
+                    <Button
+                        title="LandLeft"
+                        onPress={() => {
+                            setOrientation('landscape-left');
+                        }}
+                    />
+                    <Button
+                        title="LandRight"
+                        onPress={() => {
+                            setOrientation('landscape-right');
+                        }}
+                    />
+                    <Button
+                        title="Port"
+                        onPress={() => {
+                            setOrientation('portrait');
+                        }}
+                    />
+                    <Button
+                        title="PortDown"
+                        onPress={() => {
+                            setOrientation('portrait-upside-down');
+                        }}
+                    />
+                </View>
+            </View>
+            <ScrollView
+                style={{
+                    position: 'absolute',
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    height: 200,
+                    backgroundColor: 'black',
+                }}
+                horizontal={true}
+            >
+                {images.map((image, index) => {
+                    return (
+                        <View key={index} style={{ flex: 1 }}>
+                            <Image
+                                style={{ flex: 1 }}
+                                source={{
+                                    uri: image.uri,
+                                }}
+                                height={200}
+                                width={200}
+                            />
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+                            <Text
+                                style={{
+                                    position: 'absolute',
+                                    bottom: 0,
+                                    left: 0,
+                                    right: 0,
+                                    height: 20,
+                                    color: 'red',
+                                }}
+                            >
+                                VC: {image.visionCameraWidth}x{image.visionCameraHeight}. RN:{' '}
+                                {image.imageWidth}x{image.imageheight}
+                            </Text>
+                        </View>
+                    );
+                })}
+            </ScrollView>
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+    );
+};
